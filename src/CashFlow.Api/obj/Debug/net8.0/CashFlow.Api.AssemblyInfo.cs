0146d02626af5d3640cbde43c25953a3778f0084
van-gomes <nninha.gomes@gmail.com>
2025-04-29 14:58:52 -0300

    feat: improve error responses

--- src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2c0c1c844273f2dbc0ef5dca8af7a76f52eb46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c617138350c3df6ccb30627a13cdc543958344c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
