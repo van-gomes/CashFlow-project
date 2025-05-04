2f5c8654f4ad13cac13de3c1bd3775e26b4e7c73
van-gomes <nninha.gomes@gmail.com>
2025-05-04 18:50:54 -0300

    feat: add translation French

--- src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627da8ff2ec0254fe591e166117c60d8f033333f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d243b311d73ddd4db457db97f16f5c43eba384a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
