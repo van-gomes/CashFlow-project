7fc6e2b7c4ab4b883c46f5abf785823a3869915e
van-gomes <nninha.gomes@gmail.com>
2025-05-10 18:16:28 -0300

    feat: first sucess test

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,30 @@
+using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Description",
+            Title = "Apple",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
--- tests/Validators.Tests/obj/Debug/net8.0/Validators.Tests.AssemblyInfo.cs
+++ tests/Validators.Tests/obj/Debug/net8.0/Validators.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216679f083ae45ebff0e6d51b6cbad6732344dd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c3dd0b90da710f9f9c716e2efdc5c3cb61d775")]
 [assembly: System.Reflection.AssemblyProductAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
