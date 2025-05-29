7d3b19f50cebecd413122d8c06ae00646106e282
van-gomes <nninha.gomes@gmail.com>
2025-05-29 12:54:15 -0300

    feat: create database context SQL Server

--- src/CashFlow.Communication/obj/Debug/net8.0/CashFlow.Communication.AssemblyInfo.cs
+++ src/CashFlow.Communication/obj/Debug/net8.0/CashFlow.Communication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b42b35bf3d1ddea4cd4a01d99f67cbf2df01788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba1b16a8831ec36fb9b76b342c2a14bc9abe90a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -1,6 +1,15 @@
+using CashFlow.Domain;
+using Microsoft.EntityFrameworkCore;
+
 namespace CashFlow.Infrastructure.DataAccess;
 
-public class CashFlowDbContext
+public class CashFlowDbContext : DbContext
 {
-    
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashFlowdb;UserId=;Password=;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 }
