a327d2cc662a637d4e042c22a819afbe29198714
van-gomes <nninha.gomes@gmail.com>
2026-02-28 18:18:01 -0300

    feat: create spreadsheet and configure fonts

---
+++ backend/src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,19 @@
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Register.Reports.Excel;
+
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var workbook = new XLWorkbook();
+
+        workbook.Author = "Vania Gomes";
+        workbook.Style.Font.FontSize = 12;
+        workbook.Style.Font.FontName = "Times New Roman";
+
+        var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+        
+        return new byte[] { };
+    }
+}
