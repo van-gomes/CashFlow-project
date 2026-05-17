106a4d9635358d18a1375e42bb3e1f3756c9df2c
van-gomes <nninha.gomes@gmail.com>
2026-05-17 20:27:48 -0300

    create first migration

--- backend/src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
+++ backend/src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f045be7ef8e942b53eb26b43841ccc95c5ebb77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032bb1069956bb303f19e17249becafecd030410")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/src/CashFlow.Exception/obj/Debug/net8.0/pt-BR/CashFlow.Exception.resources.cs
+++ backend/src/CashFlow.Exception/obj/Debug/net8.0/pt-BR/CashFlow.Exception.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0290a7a54404dd1cfb391c75c2564aaf85dc2ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032bb1069956bb303f19e17249becafecd030410")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
