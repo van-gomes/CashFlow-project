9e14ba5e63df4cceaccbdd557ac16a24b4cb4ce1
van-gomes <nninha.gomes@gmail.com>
2026-06-17 16:37:03 -0300

    feat: complete login

--- backend/src/CashFlow.Application/DependencyInjectionExtension.cs
+++ backend/src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -6,6 +6,7 @@ using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Application.UseCases.Expenses.Register.Reports.Excel;
 using CashFlow.Application.UseCases.Expenses.Register.Reports.PDF;
 using CashFlow.Application.UseCases.Expenses.Update;
+using CashFlow.Application.UseCases.Login.DoLogin;
 using CashFlow.Application.UseCases.Users.Register;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,5 +34,6 @@ public static class DependencyInjectionExtension
         services.AddScoped<IGenerateExpensesReportExcelUseCase, GenerateExpensesReportExcelUseCase>();
         services.AddScoped<IGenerateExpensesReportPdfUseCase, GenerateExpensesReportPdfUseCase>();
         services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        services.AddScoped<IDoLoginUseCase, DoLoginUseCase>();
     }
 }
