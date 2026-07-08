df68959ee04a142417834934cba4dd858e0d8422
van-gomes <nninha.gomes@gmail.com>
2026-07-08 15:55:51 -0300

    feat: understand and create mock

--- backend/src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ backend/src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebfca8d3ded33330c4d2c4bcf8754535f0226b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a48b1ce3f6bb6721b15d688868e2b06dfeb843")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/tests/UseCases.Test/obj/Debug/net8.0/UseCases.Test.AssemblyInfo.cs
+++ backend/tests/UseCases.Test/obj/Debug/net8.0/UseCases.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UseCases.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebfca8d3ded33330c4d2c4bcf8754535f0226b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a48b1ce3f6bb6721b15d688868e2b06dfeb843")]
 [assembly: System.Reflection.AssemblyProductAttribute("UseCases.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UseCases.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
