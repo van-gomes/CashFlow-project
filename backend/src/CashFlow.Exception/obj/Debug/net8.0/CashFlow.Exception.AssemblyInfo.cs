4bd2161bfe2091aabe8438f51eb09d19ac917707
van-gomes <nninha.gomes@gmail.com>
2026-07-14 07:40:18 -0300

    feat: set up test environment

--- backend/src/CashFlow.Exception/obj/Debug/net8.0/CashFlow.Exception.AssemblyInfo.cs
+++ backend/src/CashFlow.Exception/obj/Debug/net8.0/CashFlow.Exception.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a22723913c6e24ac9ff98ce0482ef288f3bbda0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab4289f7f842955a3363de4783ba80dfd4d2442")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
