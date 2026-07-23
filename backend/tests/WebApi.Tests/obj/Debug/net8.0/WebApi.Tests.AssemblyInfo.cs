df28eac5deed6c39fc6d22339fef4438cd4961d4
van-gomes <nninha.gomes@gmail.com>
2026-07-23 16:51:30 -0300

    feat: refactor integration tests using Factory class

--- backend/tests/WebApi.Tests/obj/Debug/net8.0/WebApi.Tests.AssemblyInfo.cs
+++ backend/tests/WebApi.Tests/obj/Debug/net8.0/WebApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba16c373c44b884ccba081f1ca61c190945c29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e365a4e1113a1c4c5e513c185defc56a7754512")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
